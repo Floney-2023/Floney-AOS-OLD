3e370f902e8f672250d9a49f82c3ffd86c505f1a
wnsgml517 <wnsgml517@naver.com>
2024-02-05 01:22:09 +0900

    [UI] add bottom navigation bar design

--- app/src/main/java/com/aos/floney/presentation/HomeActivity.kt
+++ app/src/main/java/com/aos/floney/presentation/HomeActivity.kt
@@ -1,4 +1,28 @@
 package com.aos.floney.presentation
 
-class HomeActivity {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.aos.floney.R
+import com.aos.floney.databinding.ActivityHomeBinding
+import com.aos.floney.presentation.home.HomeFragment
+import kr.ac.konkuk.gdsc.plantory.util.binding.BindingActivity
+
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.bottomNavigationView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+
+    }
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.fcv_main, T::class.simpleName)
+        }
+    }
 }
