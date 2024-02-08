a50b2a50dc1ca3955f458a296a89fe207400e5b8
wnsgml517 <wnsgml517@naver.com>
2024-02-09 00:50:29 +0900

    [ui/#5] floating button 구현
    제보하기 버튼

--- app/src/main/java/com/aos/floney/presentation/HomeActivity.kt
+++ app/src/main/java/com/aos/floney/presentation/HomeActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.aos.floney.R
 import com.aos.floney.databinding.ActivityHomeBinding
 import com.aos.floney.presentation.home.HomeFragment
@@ -15,14 +16,26 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+        settingNavigationbar()
 
     }
     private inline fun <reified T : Fragment> navigateTo() {
         supportFragmentManager.commit {
             replace<T>(R.id.nav_host_fragment, T::class.simpleName)
         }
+    }
+    fun settingNavigationbar(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // 가운데 메뉴(제보하기)에 대한 터치 이벤트를 막기 위한 로직
+        binding.bottomNavigationView.apply {
+            setupWithNavController(navController)
+            background = null
+            menu.getItem(2).isEnabled = false
+        }
+        //NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+
+
     }
 }
