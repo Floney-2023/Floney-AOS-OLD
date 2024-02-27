36f7b38dd788244cf2b49d57159e7bf50cefd456
wnsgml517 <wnsgml517@naver.com>
2024-02-27 15:10:07 +0900

    [ui/#20] fix login 화면

--- app/src/main/java/com/aos/floney/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/aos/floney/presentation/login/LoginActivity.kt
@@ -1,27 +1,31 @@
 package com.aos.floney.presentation.login
 
 import android.os.Bundle
-import android.text.TextUtils.replace
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.aos.floney.R
-import com.aos.floney.databinding.ActivityMainBinding
+import com.aos.floney.databinding.ActivityLoginBinding
 import com.aos.floney.presentation.home.HomeFragment
 import com.aos.floney.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navigateTo<HomeFragment>()
+
+
+        binding.startLogin.setOnClickListener{
+            navigateTo<HomeFragment>()
+        }
+
     }
 
     private inline fun <reified T : Fragment> navigateTo() {
         supportFragmentManager.commit {
-            replace<T>(R.id.fcv_main, T::class.simpleName)
+            replace<T>(R.id.nav_host_fragment, T::class.simpleName)
         }
     }
 }
