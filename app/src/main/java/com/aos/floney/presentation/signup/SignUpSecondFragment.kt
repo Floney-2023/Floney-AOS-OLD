01753259b9079a6fd6add3780dd09d1e06966c98
wnsgml517 <wnsgml517@naver.com>
2024-03-15 23:12:23 +0900

    [feat/#27] toast 조건 추가 - 유효 이메일 형식
    2번째 회원가입 화면

--- app/src/main/java/com/aos/floney/presentation/signup/SignUpSecondFragment.kt
+++ app/src/main/java/com/aos/floney/presentation/signup/SignUpSecondFragment.kt
@@ -1,6 +1,7 @@
 package com.aos.floney.presentation.signup
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,12 +22,21 @@ class SignUpSecondFragment : BindingFragment<FragmentSignupSecondBinding>(R.layo
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.back.setOnClickListener {
-            findNavController().popBackStack()
-        }
         binding.sendMail.setOnClickListener {
-            findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+            val email = binding.idText.text.toString().trim() // EditText에서 이메일 주소 가져오기
+
+            if (isEmailValid(email)) {
+                // 이메일 형식이 올바르면 다음으로 이동
+                findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+            } else {
+                // 이메일 형식이 올바르지 않은 경우 사용자에게 알림
+                // 예를 들어 Toast 메시지를 사용하여 알림을 표시할 수 있습니다.
+                // Toast.makeText(requireContext(), "올바른 이메일 형식이 아닙니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+    private fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
 
