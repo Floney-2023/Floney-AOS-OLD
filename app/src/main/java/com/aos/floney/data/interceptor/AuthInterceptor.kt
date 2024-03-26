55d8df5d4369afdaf9024805de65142442a2cf56
wnsgml517 <wnsgml517@naver.com>
2024-03-26 23:06:33 +0900

    [feat/#27] 토큰 만료 시 재발급  (추후수정)
    값은 서버로부터 잘 읽어왔으나, 토큰 값이 빈값으로 저장되는 이슈 발생

--- app/src/main/java/com/aos/floney/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/aos/floney/data/interceptor/AuthInterceptor.kt
@@ -12,6 +12,7 @@ import kotlinx.serialization.json.Json
 import com.aos.floney.domain.repository.DataStoreRepository
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -32,7 +33,7 @@ class AuthInterceptor @Inject constructor(
             originalRequest.url.encodedPath.equals("/users/logout",true)||
             originalRequest.url.encodedPath.equals("/users/email/mail",true)||
             originalRequest.url.encodedPath.equals("/users",true)
-            ){
+        ){
             val headerRequest = originalRequest.newAuthBuilder()
                 .build()
             val response = chain.proceed(headerRequest)
@@ -61,6 +62,7 @@ class AuthInterceptor @Inject constructor(
                 }
 
                 CODE_INVALID_USER -> {
+                    Timber.e("유효하지 않은 사용자")
                     saveAccessToken("", "")
                 }
             }
@@ -93,31 +95,26 @@ class AuthInterceptor @Inject constructor(
 
         Timber.e("리프레시 토큰?? ")
         val refreshTokenRequest = originalRequest.newBuilder()
-            .post(createTokenReissueRequestBody())
             .url("${BASE_URL}users/reissue")
+            .post(createTokenReissueRequestBody())
             .build()
-        val refreshTokenResponse = chain.proceed(refreshTokenRequest)
-
-        if (refreshTokenResponse.isSuccessful) {
-            val responseToken = json.decodeFromString(
-                refreshTokenResponse.body?.string().toString()
-            ) as BaseResponse<ResponseReIssueTokenDto>
-            Timber.e("리프레시 토큰! : ${responseToken.data}")
-            if (responseToken.data != null) {
-
-                saveAccessToken(
-                    responseToken.data.accessToken,
-                    responseToken.data.refreshToken
-                )
+        OkHttpClient().newCall(refreshTokenRequest).execute().use { refreshTokenResponse ->
+            if (refreshTokenResponse.code == 201) {
+                val responseBody = refreshTokenResponse.body?.string()
+                responseBody?.let { body ->
+                    val responseToken = json.decodeFromString<ResponseReIssueTokenDto>(body)
+                    Timber.e("리프레시 토큰?! ${responseToken}")
+                    saveAccessToken(responseToken.accessToken, responseToken.refreshToken)
+                }
+                refreshTokenResponse.close()
+                val newRequest = originalRequest.newAuthBuilder().build()
+                return chain.proceed(newRequest)
+            } else {
+                refreshTokenResponse.close()
+                Timber.e("리프레시 토큰 실패 : ${refreshTokenResponse.code}")
+                saveAccessToken("", "")
+                return chain.proceed(headerRequest)
             }
-            refreshTokenResponse.close()
-            val newRequest = originalRequest.newAuthBuilder().build()
-            return chain.proceed(newRequest)
-        } else {
-            refreshTokenResponse.close()
-            Timber.e("리프레시 토큰?_? : ${refreshTokenResponse.code}")
-            saveAccessToken("", "")
-            return chain.proceed(headerRequest)
         }
     }
     private fun createTokenReissueRequestBody(): RequestBody {
